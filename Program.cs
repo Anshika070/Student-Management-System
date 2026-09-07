d2961ff7f803627a3ffca585a4d2a3aa32f88272
Kumari Anshika <129029842+Anshika070@users.noreply.github.com>
2026-09-07 17:07:51 +0000

    Created student management system

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Student student = new Student("Anshika", 35);
+
+        Console.WriteLine(student.Name);
+        Console.WriteLine(student.Marks);
+
+        if (student.HasPassed())
+        {
+            Console.WriteLine("Student Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Student Failed!");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,16 @@
+class Student
+{
+    public string Name;
+    public int Marks;
+
+    public Student(string name, int marks)
+    {
+        Name = name;
+        Marks = marks;
+    }
+
+    public bool HasPassed()
+    {
+        return Marks >= 40;
+    }
+}
